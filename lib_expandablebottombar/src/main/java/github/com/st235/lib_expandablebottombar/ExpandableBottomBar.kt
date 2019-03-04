0dba30798b0f0b8ef6aea3bfd35aaae0714ac2b0
Alexander Dadukin <st235@yandex.ru>
2019-03-05 00:03:02 +0300

    implemented select by api.

--- lib_expandablebottombar/src/main/java/github/com/st235/lib_expandablebottombar/ExpandableBottomBar.kt
+++ lib_expandablebottombar/src/main/java/github/com/st235/lib_expandablebottombar/ExpandableBottomBar.kt
@@ -3,28 +3,15 @@ package github.com.st235.lib_expandablebottombar
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.Typeface.BOLD
 import android.support.annotation.ColorInt
 import android.support.annotation.IdRes
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
-import android.support.v4.content.ContextCompat
-import android.support.v7.widget.AppCompatImageView
-import android.support.v7.widget.AppCompatTextView
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.StyleSpan
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.util.AttributeSet
-import android.view.Gravity
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.TextView
-import github.com.st235.lib_expandablebottombar.utils.DrawableHelper
-import github.com.st235.lib_expandablebottombar.utils.applyForApiMAndHigher
-import github.com.st235.lib_expandablebottombar.utils.createChain
-import github.com.st235.lib_expandablebottombar.utils.toPx
+import github.com.st235.lib_expandablebottombar.utils.*
 
 internal const val ITEM_NOT_SELECTED = -1
 
@@ -51,8 +38,7 @@ class ExpandableBottomBar @JvmOverloads constructor(
     private var transitionDuration: Int = 0
 
     @IdRes private var selectedItemId: Int = ITEM_NOT_SELECTED
-
-    private lateinit var items: List<ExpandableBottomBarMenuItem> items
+    private val viewControllers: MutableMap<Int, ExpandableItemViewController> = mutableMapOf()
 
     var onItemClickListener: OnItemClickListener? = null
 
@@ -91,47 +77,54 @@ class ExpandableBottomBar @JvmOverloads constructor(
     }
 
     fun addItems(items: List<ExpandableBottomBarMenuItem>) {
-        this.items = items
-
         val firstItemId = items.first().itemId
         val lastItemId = items.last().itemId
         selectedItemId = firstItemId
 
         for (i in 0 until items.size) {
             val item = items[i]
-            val itemView = createItem(item)
+            val viewController = createItem(item)
+            viewControllers[item.itemId] = viewController
 
             val prevIconId = if (i - 1 < 0) firstItemId else items[i - 1].itemId
             val nextIconId = if (i + 1 >= items.size) lastItemId else items[i + 1].itemId
 
-            addItemInternal(itemView, prevIconId, nextIconId)
+            viewController.attachTo(
+                this,
+                prevIconId, nextIconId,
+                menuItemHorizontalMargin, menuItemVerticalMargin
+            )
         }
     }
 
-    fun selectById(id: Int) {
-        val itemToSelect = items.find { it.itemId == id }!!
-        selectByItem(itemToSelect)
+    fun select(@IdRes id: Int) {
+        val itemToSelect = viewControllers.getValue(id)
+        onItemSelected(itemToSelect.menuItem)
     }
 
-    fun selectByIndex(index: Int) {
-        val itemToSelect = items[index]
-        selectByItem(itemToSelect)
+    private fun createItem(menuItem: ExpandableBottomBarMenuItem): ExpandableItemViewController {
+        val colors = intArrayOf(menuItem.activeColor, inactiveBackgroundColor)
+        val selectedStateColorList = ColorStateList(backgroundStates, colors)
+
+        val viewController =
+            ExpandableItemViewController.Builder(menuItem)
+                .itemMargins(menuHorizontalPadding, menuVerticalPadding)
+                .itemBackground(backgroundCornerRadius, backgroundOpacity)
+                .itemsColors(selectedStateColorList)
+                .onItemClickListener { v: View ->
+                    onItemSelected(menuItem)
+                    onItemClickListener?.invoke(v, menuItem)
+                }
+                .build(context)
+
+        if (selectedItemId == menuItem.itemId) {
+            viewController.selectItem()
+        }
+
+        return viewController
     }
 
-    fun selectByItem(itemToSelect: ExpandableBottomBarMenuItem) {
-        val previouslyItemView = findViewById<LinearLayout>(itemToSelect.itemId)
-        val previouslyTextView = previouslyItemView.findViewWithTag<View>(TEXT_TAG) as TextView
-        val previouslyActiveIconView = previouslyItemView.findViewWithTag<View>(IMAGE_TAG) as AppCompatImageView
-        applyTransition()
-        selectItem(itemToSelect, previouslyItemView, previouslyActiveIconView, previouslyTextView)
-    }
-
-    private fun onItemClickInternal(
-        activeMenuItem: ExpandableBottomBarMenuItem,
-        activeItemView: View,
-        activeIconView: AppCompatImageView,
-        activeTextView: TextView
-    ) {
+    private fun onItemSelected(activeMenuItem: ExpandableBottomBarMenuItem) {
         if (selectedItemId == activeMenuItem.itemId) {
             return
         }
@@ -141,128 +134,11 @@ class ExpandableBottomBar @JvmOverloads constructor(
         val set = ConstraintSet()
         set.clone(this)
 
-        selectItem(activeMenuItem, activeItemView, activeIconView, activeTextView)
-
-        val previouslyItemView = findViewById<LinearLayout>(selectedItemId)
-        val previouslyTextView = previouslyItemView.findViewWithTag<View>(TEXT_TAG) as TextView
-        val previouslyActiveIconView = previouslyItemView.findViewWithTag<View>(IMAGE_TAG) as AppCompatImageView
-
-        deselectItem(previouslyItemView, previouslyActiveIconView, previouslyTextView)
-
+        viewControllers.getValue(activeMenuItem.itemId).selectItem()
+        viewControllers.getValue(selectedItemId).deselectItem()
         selectedItemId = activeMenuItem.itemId
+
         set.applyTo(this)
-    }
-
-    private fun createItem(menuItem: ExpandableBottomBarMenuItem): View {
-        val itemView = LinearLayout(context).apply {
-            id = menuItem.itemId
-            orientation = LinearLayout.HORIZONTAL
-            setPadding(menuHorizontalPadding, menuVerticalPadding,
-                    menuHorizontalPadding, menuVerticalPadding)
-        }
-
-        val colors = intArrayOf(menuItem.activeColor, inactiveBackgroundColor)
-        val selectedStateColorList = ColorStateList(backgroundStates, colors)
-
-        val iconView = AppCompatImageView(context).apply {
-            tag = IMAGE_TAG
-            setImageDrawable(DrawableHelper.createDrawable(context, menuItem.iconId, selectedStateColorList))
-        }
-
-        val textView = AppCompatTextView(context).apply {
-            val rawText = SpannableString(resources.getString(menuItem.textId))
-            rawText.setSpan(StyleSpan(BOLD), 0, rawText.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-            setTextColor(selectedStateColorList)
-            text = rawText
-            tag = TEXT_TAG
-            gravity = Gravity.CENTER
-            visibility = View.GONE
-            textSize = 15F
-        }
-
-        val textLayoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.MATCH_PARENT
-        ).apply {
-            gravity = Gravity.CENTER
-            setMargins(8.toPx(), 0, 0, 0)
-        }
-
-        if (selectedItemId == menuItem.itemId) {
-            selectItem(menuItem, itemView, iconView, textView)
-        }
-
-        with(itemView) {
-            addView(iconView)
-            addView(textView, textLayoutParams)
-            setOnClickListener {
-                onItemClickInternal(menuItem, itemView, iconView, textView)
-                onItemClickListener?.invoke(itemView, menuItem)
-            }
-        }
-
-        return itemView
-    }
-
-    private fun addItemInternal(
-        itemView: View,
-        previousIconId: Int,
-        nextIconId: Int
-    ) {
-        val lp = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT
-        )
-
-        lp.setMargins(menuItemHorizontalMargin, menuItemVerticalMargin,
-                menuItemHorizontalMargin, menuItemVerticalMargin)
-
-        addView(itemView, lp)
-
-        val cl = ConstraintSet()
-        cl.clone(this)
-
-        cl.connect(itemView.id, ConstraintSet.TOP, id, ConstraintSet.TOP)
-        cl.connect(itemView.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
-
-        if (previousIconId == itemView.id) {
-            cl.connect(itemView.id, ConstraintSet.START, id, ConstraintSet.START)
-        } else {
-            cl.connect(itemView.id, ConstraintSet.START, previousIconId, ConstraintSet.END)
-            cl.createChain(previousIconId, itemView.id, ConstraintSet.CHAIN_PACKED)
-        }
-
-        if (nextIconId == itemView.id) {
-            cl.connect(itemView.id, ConstraintSet.END, id, ConstraintSet.END)
-        } else {
-            cl.connect(itemView.id, ConstraintSet.END, nextIconId, ConstraintSet.START)
-            cl.createChain(itemView.id, nextIconId, ConstraintSet.CHAIN_PACKED)
-        }
-
-        cl.applyTo(this)
-    }
-
-    private fun deselectItem(
-        itemView: View,
-        iconView: AppCompatImageView,
-        textView: TextView
-    ) {
-        itemView.background = null
-        textView.visibility = View.GONE
-        textView.isSelected = false
-        iconView.isSelected = false
-    }
-
-    private fun selectItem(
-        menuItem: ExpandableBottomBarMenuItem,
-        itemView: View,
-        iconView: AppCompatImageView,
-        textView: TextView
-    ) {
-        itemView.background =
-            DrawableHelper.createShapeDrawable(menuItem.activeColor, backgroundCornerRadius, backgroundOpacity)
-        textView.visibility = View.VISIBLE
-        textView.isSelected = true
-        iconView.isSelected = true
     }
 
     private fun applyTransition() {
---
+++ lib_expandablebottombar/src/main/java/github/com/st235/lib_expandablebottombar/ExpandableItemViewController.kt
@@ -0,0 +1,177 @@
+package github.com.st235.lib_expandablebottombar
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Typeface
+import android.support.annotation.Px
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.support.v7.widget.AppCompatImageView
+import android.support.v7.widget.AppCompatTextView
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import github.com.st235.lib_expandablebottombar.utils.DrawableHelper
+import github.com.st235.lib_expandablebottombar.utils.createChain
+import github.com.st235.lib_expandablebottombar.utils.toPx
+
+internal class ExpandableItemViewController(
+    internal val menuItem: ExpandableBottomBarMenuItem,
+    private val itemView: View,
+    private val textView: TextView,
+    private val iconView: ImageView,
+    private val backgroundCornerRadius: Float,
+    private val backgroundOpacity: Float
+) {
+
+    fun deselectItem() {
+        itemView.background = null
+        textView.visibility = View.GONE
+        textView.isSelected = false
+        iconView.isSelected = false
+    }
+
+    fun selectItem() {
+        itemView.background =
+            DrawableHelper.createShapeDrawable(
+                menuItem.activeColor,
+                backgroundCornerRadius,
+                backgroundOpacity
+            )
+        textView.visibility = View.VISIBLE
+        textView.isSelected = true
+        iconView.isSelected = true
+    }
+
+    fun attachTo(parent: ConstraintLayout,
+                 previousIconId: Int,
+                 nextIconId: Int,
+                 menuItemHorizontalMargin: Int,
+                 menuItemVerticalMargin: Int) {
+        val lp = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        lp.setMargins(menuItemHorizontalMargin, menuItemVerticalMargin,
+            menuItemHorizontalMargin, menuItemVerticalMargin)
+
+        parent.addView(itemView, lp)
+
+        val cl = ConstraintSet()
+        cl.clone(parent)
+
+        cl.connect(itemView.id, ConstraintSet.TOP, parent.id, ConstraintSet.TOP)
+        cl.connect(itemView.id, ConstraintSet.BOTTOM, parent.id, ConstraintSet.BOTTOM)
+
+        if (previousIconId == itemView.id) {
+            cl.connect(itemView.id, ConstraintSet.START, parent.id, ConstraintSet.START)
+        } else {
+            cl.connect(itemView.id, ConstraintSet.START, previousIconId, ConstraintSet.END)
+            cl.createChain(previousIconId, itemView.id, ConstraintSet.CHAIN_PACKED)
+        }
+
+        if (nextIconId == itemView.id) {
+            cl.connect(itemView.id, ConstraintSet.END, parent.id, ConstraintSet.END)
+        } else {
+            cl.connect(itemView.id, ConstraintSet.END, nextIconId, ConstraintSet.START)
+            cl.createChain(itemView.id, nextIconId, ConstraintSet.CHAIN_PACKED)
+        }
+
+        cl.applyTo(parent)
+    }
+
+    class Builder(private val menuItem: ExpandableBottomBarMenuItem) {
+
+        @Px
+        private var itemVerticalPadding: Int = 0
+        @Px
+        private var itemHorizontalPadding: Int = 0
+        @Px
+        private var backgroundCornerRadius: Float = 0.0f
+        @Px
+        private var backgroundOpacity: Float = 1.0f
+
+        private lateinit var backgroundColorSelector: ColorStateList
+        private lateinit var onItemClickListener: (View) -> Unit
+
+        fun itemMargins(
+            @Px itemHorizontalPadding: Int,
+            @Px itemVerticalPadding: Int
+        ): Builder {
+            this.itemVerticalPadding = itemVerticalPadding
+            this.itemHorizontalPadding = itemHorizontalPadding
+            return this
+        }
+
+        fun itemBackground(backgroundCornerRadius: Float, backgroundOpacity: Float): Builder {
+            this.backgroundCornerRadius = backgroundCornerRadius
+            this.backgroundOpacity = backgroundOpacity
+            return this
+        }
+
+        fun itemsColors(backgroundColorSelector: ColorStateList): Builder {
+            this.backgroundColorSelector = backgroundColorSelector
+            return this
+        }
+
+        fun onItemClickListener(onItemClickListener: (View) -> Unit): Builder {
+            this.onItemClickListener = onItemClickListener
+            return this
+        }
+
+        fun build(context: Context): ExpandableItemViewController {
+            val itemView = LinearLayout(context).apply {
+                id = menuItem.itemId
+                orientation = LinearLayout.HORIZONTAL
+                setPadding(itemHorizontalPadding, itemVerticalPadding, itemHorizontalPadding, itemVerticalPadding)
+            }
+
+            val iconView = AppCompatImageView(context).apply {
+                tag = IMAGE_TAG
+                setImageDrawable(
+                    DrawableHelper.createDrawable(
+                        context,
+                        menuItem.iconId,
+                        backgroundColorSelector
+                    )
+                )
+            }
+
+            val textView = AppCompatTextView(context).apply {
+                val rawText = SpannableString(resources.getString(menuItem.textId))
+                rawText.setSpan(StyleSpan(Typeface.BOLD), 0, rawText.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                setTextColor(backgroundColorSelector)
+                text = rawText
+                tag = TEXT_TAG
+                gravity = Gravity.CENTER
+                visibility = View.GONE
+                textSize = 15F
+            }
+
+            val textLayoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            ).apply {
+                gravity = Gravity.CENTER
+                setMargins(8.toPx(), 0, 0, 0)
+            }
+
+            with(itemView) {
+                addView(iconView)
+                addView(textView, textLayoutParams)
+                setOnClickListener(onItemClickListener)
+            }
+
+            return ExpandableItemViewController(
+                menuItem,
+                itemView, textView, iconView,
+                backgroundCornerRadius, backgroundOpacity
+            )
+        }
+    }
+}
