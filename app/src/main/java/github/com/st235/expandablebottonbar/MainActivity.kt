7832bd0fd611d927f4c46ececd238ae0f7e9ad73
Alexander Dadukin <st235@yandex.ru>
2019-03-05 00:03:14 +0300

    fixed sample app.

--- app/src/main/java/github/com/st235/expandablebottonbar/MainActivity.kt
+++ app/src/main/java/github/com/st235/expandablebottonbar/MainActivity.kt
@@ -23,16 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         bottomBar.addItems(
                 ExpandableBottomBarMenuItem.Builder()
-                        .addItem(R.id.icon_home, R.drawable.ic_bug, R.string.text, Color.GRAY)
-                        .addItem(R.id.icon_go, R.drawable.ic_gift, R.string.text2, Color.parseColor("#ff77a9"))
-                        .addItem(R.id.icon_left, R.drawable.ic_one, R.string.text3, Color.parseColor("#58a5f0"))
-                        .addItem(R.id.icon_right, R.drawable.ic_two, R.string.text4, Color.parseColor("#be9c91"))
+                        .addItem(R.id.icon_home, R.drawable.ic_home, R.string.text, Color.GRAY)
+                        .addItem(R.id.icon_likes, R.drawable.ic_likes, R.string.text2, Color.parseColor("#ff77a9"))
+                        .addItem(R.id.icon_bookmarks, R.drawable.ic_bookmarks, R.string.text3, Color.parseColor("#58a5f0"))
+                        .addItem(R.id.icon_settings, R.drawable.ic_settings, R.string.text4, Color.parseColor("#be9c91"))
                         .build()
         )
 
         bottomBar.onItemClickListener = { v, i ->
-            val anim = ViewAnimationUtils.createCircularReveal(color, bottomBar.x.toInt()
-                    + v.x.toInt(), bottomBar.y.toInt() + v.y.toInt(), 0F, findViewById<View>(android.R.id.content).height.toFloat())
+            val anim = ViewAnimationUtils.createCircularReveal(color,
+                bottomBar.x.toInt() + v.x.toInt() + v.height / 2,
+                bottomBar.y.toInt() + v.y.toInt() + v.width / 2, 0F,
+                findViewById<View>(android.R.id.content).height.toFloat())
             color.setBackgroundColor(ColorUtils.setAlphaComponent(i.activeColor, 60))
             anim.duration = 420
             anim.start()
