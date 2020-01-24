3e3c48510aa03348e264b9bbae139dfc21630693
Alexander Dadukin <hi@st235.xyz>
2020-01-24 21:28:18 +0200

    implemented java callbacks examples.

--- app/src/main/java/github/com/st235/expandablebottombar/JavaActivity.java
+++ app/src/main/java/github/com/st235/expandablebottombar/JavaActivity.java
@@ -2,11 +2,15 @@ package github.com.st235.expandablebottombar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
 import github.com.st235.lib_expandablebottombar.ExpandableBottomBarMenuItem;
 
 public class JavaActivity extends AppCompatActivity {
+
+    private static final String TAG = JavaActivity.class.getSimpleName();
 
     private ExpandableBottomBar bottomBar;
 
@@ -20,10 +24,20 @@ public class JavaActivity extends AppCompatActivity {
         bottomBar.addItems(
                 new ExpandableBottomBarMenuItem.Builder(this)
                         .addItem(R.id.icon_home, R.drawable.ic_home, R.string.text, Color.GRAY)
-                        .addItem(R.id.icon_likes, R.drawable.ic_likes, R.string.text2, Color.parseColor("#ff77a9"))
-                        .addItem(R.id.icon_bookmarks, R.drawable.ic_bookmarks, R.string.text3, Color.parseColor("#58a5f0"))
-                        .addItem(R.id.icon_settings, R.drawable.ic_settings, R.string.text4, Color.parseColor("#be9c91"))
+                        .addItem(R.id.icon_likes, R.drawable.ic_likes, R.string.text2, 0xffff77a9)
+                        .addItem(R.id.icon_bookmarks, R.drawable.ic_bookmarks, R.string.text3, 0xff58a5f0)
+                        .addItem(R.id.icon_settings, R.drawable.ic_settings, R.string.text4, 0xffbe9c91)
                         .build()
         );
+
+        bottomBar.setOnItemSelectedListener((view, item) -> {
+            Log.d(TAG, "selected: " + item.toString());
+            return null;
+        });
+
+        bottomBar.setOnItemReselectedListener((view, item) -> {
+            Log.d(TAG, "reselected: " + item.toString());
+            return null;
+        });
     }
 }
