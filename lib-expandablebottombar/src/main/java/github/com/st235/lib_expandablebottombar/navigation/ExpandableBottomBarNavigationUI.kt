150cbd48b92c0424a701af148a01ee2b23a9a594
Alexander Dadukin <hi@st235.xyz>
2021-03-28 22:25:35 +0100

    actualized navigation ui animation.

--- lib-expandablebottombar/src/main/java/github/com/st235/lib_expandablebottombar/navigation/ExpandableBottomBarNavigationUI.kt
+++ lib-expandablebottombar/src/main/java/github/com/st235/lib_expandablebottombar/navigation/ExpandableBottomBarNavigationUI.kt
@@ -2,11 +2,8 @@ package github.com.st235.lib_expandablebottombar.navigation
 
 import android.os.Bundle
 import androidx.annotation.IdRes
-import androidx.navigation.NavController
+import androidx.navigation.*
 import androidx.navigation.NavController.OnDestinationChangedListener
-import androidx.navigation.NavDestination
-import androidx.navigation.NavGraph
-import androidx.navigation.NavOptions
 import github.com.st235.lib_expandablebottombar.ExpandableBottomBar
 import github.com.st235.lib_expandablebottombar.MenuItem
 import github.com.st235.lib_expandablebottombar.R
@@ -16,8 +13,8 @@ object ExpandableBottomBarNavigationUI {
 
     @JvmStatic
     fun setupWithNavController(
-        expandableBottomBar: ExpandableBottomBar,
-        navigationController: NavController
+            expandableBottomBar: ExpandableBottomBar,
+            navigationController: NavController
     ) {
         expandableBottomBar.onItemSelectedListener = { _, menuItem ->
             onNavDestinationSelected(menuItem, navigationController)
@@ -25,23 +22,23 @@ object ExpandableBottomBarNavigationUI {
 
         val weakReference = WeakReference(expandableBottomBar)
         navigationController.addOnDestinationChangedListener(
-            object : OnDestinationChangedListener {
-                override fun onDestinationChanged(
-                    controller: NavController,
-                    destination: NavDestination, arguments: Bundle?
-                ) {
-                    val view = weakReference.get()
-                    if (view == null) {
-                        navigationController.removeOnDestinationChangedListener(this)
-                        return
-                    }
-                    for (menuItem in view.getMenuItems()) {
-                        if (destination.matchDestination(menuItem.id)) {
-                            expandableBottomBar.select(menuItem.id)
+                object : OnDestinationChangedListener {
+                    override fun onDestinationChanged(
+                            controller: NavController,
+                            destination: NavDestination, arguments: Bundle?
+                    ) {
+                        val view = weakReference.get()
+                        if (view == null) {
+                            navigationController.removeOnDestinationChangedListener(this)
+                            return
+                        }
+                        for (menuItem in view.getMenuItems()) {
+                            if (destination.matchDestination(menuItem.id)) {
+                                expandableBottomBar.select(menuItem.id)
+                            }
                         }
                     }
-                }
-            })
+                })
     }
 
     //TODO(st235): https://android.googlesource.com/platform/frameworks/support/+/refs/heads/androidx-main/navigation/navigation-ui/src/main/java/androidx/navigation/ui/NavigationUI.kt#85
@@ -50,11 +47,22 @@ object ExpandableBottomBarNavigationUI {
             navController: NavController
     ): Boolean {
         val builder = NavOptions.Builder()
-            .setLaunchSingleTop(true)
-            .setEnterAnim(R.anim.nav_default_enter_anim)
-            .setExitAnim(R.anim.nav_default_exit_anim)
-            .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
-            .setPopExitAnim(R.anim.nav_default_pop_exit_anim)
+                .setLaunchSingleTop(true)
+
+        if (navController.currentDestination!!.parent!!.findNode(item.id)
+                        is ActivityNavigator.Destination) {
+            builder
+                    .setEnterAnim(R.anim.nav_default_enter_anim)
+                    .setExitAnim(R.anim.nav_default_exit_anim)
+                    .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
+                    .setPopExitAnim(R.anim.nav_default_pop_exit_anim)
+        } else {
+            builder
+                    .setEnterAnim(R.animator.nav_default_enter_anim)
+                    .setExitAnim(R.animator.nav_default_exit_anim)
+                    .setPopEnterAnim(R.animator.nav_default_pop_enter_anim)
+                    .setPopExitAnim(R.animator.nav_default_pop_exit_anim)
+        }
 
         val topDestination = navController.findStartDestination()
 
