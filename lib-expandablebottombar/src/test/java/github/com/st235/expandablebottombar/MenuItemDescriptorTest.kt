b2edf3b4d9c7cfbf9bae315d3230b8d408de618e
Alexander Dadukin <hi@st235.xyz>
2021-04-03 11:54:47 +0100

    fixed tests.

--- lib-expandablebottombar/src/test/java/github/com/st235/expandablebottombar/MenuItemDescriptorTest.kt
+++ lib-expandablebottombar/src/test/java/github/com/st235/expandablebottombar/MenuItemDescriptorTest.kt
@@ -15,46 +15,46 @@ class MenuItemDescriptorTest {
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating empty item will rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).create()
+        val item = MenuItemDescriptor.Builder(context).build()
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating item with id only will rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(0).create()
+        val item = MenuItemDescriptor.Builder(context).id(0).build()
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating item without color and icon will rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(0).text("").create()
+        val item = MenuItemDescriptor.Builder(context).id(0).text("").build()
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating item without title and icon will rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(0).color(0).create()
+        val item = MenuItemDescriptor.Builder(context).id(0).color(0).build()
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating item without color and text will rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(0).icon(0).create()
+        val item = MenuItemDescriptor.Builder(context).id(0).icon(0).build()
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating item without color will rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(0).icon(0).text("").create()
+        val item = MenuItemDescriptor.Builder(context).id(0).icon(0).text("").build()
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating item without text rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(0).color(0).icon(0).create()
+        val item = MenuItemDescriptor.Builder(context).id(0).color(0).icon(0).build()
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test that creating item without icon will rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(0).text("").color(0).create()
+        val item = MenuItemDescriptor.Builder(context).id(0).text("").color(0).build()
     }
 
     @Test
-    fun `test that right created item will not be rise an exception`() {
-        val item = MenuItemDescriptor.BuildRequest(builder, context).id(1).text("").icon(1).color(1).create()
+    fun `test that right buildd item will not be rise an exception`() {
+        val item = MenuItemDescriptor.Builder(context).id(1).text("").icon(1).color(1).build()
     }
 }
